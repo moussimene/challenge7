54c8a51871edfd419e8d78af9f81c31c61f5bac3
moussimene <mlle.moussaoui.imene@gmail.com>
2024-03-24 12:13:59 +0100

    4ieme challenge du challenge 7

---
+++ challenge four.js
@@ -0,0 +1,53 @@
+
+function checkSeason(mois){
+
+    switch(mois) {
+        case "Janvier":
+            console.log("it's winter")
+          break;
+        case "Fevrier":
+            console.log("it's winter")
+          break;
+
+        case "Mars":
+            console.log("it's spring")
+        break;
+
+        case "Avril":
+            console.log("it's spring")
+        break;
+
+        case "May":
+            console.log("it's spring")
+        break;
+         
+        case "Juin":
+            console.log("it's Summer")
+        break;
+
+        case "Juillet":
+            console.log("it's Summer")
+        break;
+        
+        case "Aout":
+            console.log("it's Summer")
+        break;
+
+        case "Septembre":
+            console.log("it's Automn")
+        break;
+
+        case "Octobre":
+            console.log("it's Automn")
+        break;
+
+        case "Novembre":
+            console.log("it's Automn")
+        break;
+
+        case "Decembre":
+            console.log("it's winter")
+        break;
+      }
+}
+checkSeason("Mars")
